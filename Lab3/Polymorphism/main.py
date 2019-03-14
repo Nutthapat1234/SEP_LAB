17bf830935233df2527574be20c5287f1309d836
Jirapat K <jirapat0811@gmail.com>
2019-03-14 17:55:57 +0700

    Add main

---
+++ Lab3/Polymorphism/main.py
@@ -0,0 +1,13 @@
+from Lab3.Polymorphism.incomplete_code import *
+
+if __name__ == '__main__':
+    travel_cost = 0
+
+    trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+             Taxi("KMITL SCB Bank","Ladkrabang Station",5),
+             Train("Ladkrabang Station","Payathai Station",40,6),
+             Taxi("Payathai Station","The British Council",3) ]
+
+    for travel in trip:
+       travel_cost += travel.find_cost()
+    print(travel_cost)
